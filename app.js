0d4a852ba2538ed982a4fd98c059498019697497
mshehzanakhtar <duaeiqra@77gmail.com>
2025-11-07 18:55:07 +0500

    feat

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
-let age =20;
-if(age>=18){
-    console.log("You are an adult.");
-}else{
-    console.log("You are a minor.");
+// let age = 99;
+// if (age >= 18) {
+//     console.log ("You are an adult.");
+// } else {
+//     console.log ("You are a minor.");
+// }
+// let marks = 100;
+// if (marks >= 90) {
+//     console.log ("Excellent");
+// } else if (marks >= 80) {
+//     console.log ("A");
+// } else {
+// //     console.log ("Needs Improvement");
+// }
+ let marks = prompt("Enter your marks:");
+if (marks < 0 || marks > 100) {
+    alert("Invalid marks entered.");
+} else if (marks >= 90) {
+    alert("Grade: A");
+} else if (marks >= 80) {
+    alert("Grade: B");
+} else if (marks >= 70) {
+    alert("Grade: C");
+} else if (marks >= 60) {
+    alert("Grade: D");
+} else {
+    alert("Grade: F");
+}
